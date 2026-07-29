188e9a42944029e6321bc5e833eeb23586769f12
Max <kiporenkomaks8@gmail.com>
2026-07-29 14:02:43 +0300

    add serializers, views and path to urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,27 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    CinemaHallViewSet,
+    GenreViewSet,
+    ActorViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+    OrderViewSet,
+    TicketViewSet
+)
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+router.register("cinema-halls", CinemaHallViewSet)
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie-sessions", MovieSessionViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
